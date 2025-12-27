3b1d24230743b8a7e2dc6a241a417b59b26d8fee
Ramkrishna97 <ramkrishnagoswami10@gmail.com>
2025-12-27 10:00:53 +0530

    added Model files

--- src/main/java/com/example/candidateevaluation/model/Evaluation.java
+++ src/main/java/com/example/candidateevaluation/model/Evaluation.java
@@ -1,4 +1,52 @@
 package com.example.candidateevaluation.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import org.hibernate.annotations.ManyToAny;
+import org.hibernate.annotations.ValueGenerationType;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.sql.Time;
+@Entity
 public class Evaluation {
+    private String comments;
+    private String feedback;
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Time evaluatedAt;
+
+    public Evaluation() {
+    }
+
+    public Evaluation(String comments, String feedback, Time evaluatedAt) {
+        this.comments = comments;
+        this.feedback = feedback;
+        this.evaluatedAt = evaluatedAt;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+    public Time getEvaluatedAt() {
+        return evaluatedAt;
+    }
+
+    public void setEvaluatedAt(Time evaluatedAt) {
+        this.evaluatedAt = evaluatedAt;
+    }
 }
--- src/main/java/com/example/candidateevaluation/model/Evaluator.java
+++ src/main/java/com/example/candidateevaluation/model/Evaluator.java
@@ -1,4 +1,36 @@
 package com.example.candidateevaluation.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Evaluator {
+    @Id
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "candidate_id")
+    private Candidate candidate;
+
+    public Candidate getCandidate() {
+        return candidate;
+    }
+
+    public Evaluator() {
+    }
+
+    public void setCandidate(Candidate candidate) {
+        this.candidate = candidate;
+    }
+
+    public Evaluator(String name, Candidate candidate) {
+        this.name = name;
+        this.candidate = candidate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
