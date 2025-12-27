7c9361752dde7cbd979c7c174b37f99b6484b952
Ramkrishna97 <ramkrishnagoswami10@gmail.com>
2025-12-27 10:40:04 +0530

    added files

---
+++ src/main/java/com/example/candidateevaluation/CandidateEvaluationApplication.java
@@ -0,0 +1,13 @@
+package com.example.candidateevaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidateEvaluationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CandidateEvaluationApplication.class, args);
+    }
+
+}
